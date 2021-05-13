16ba6b4fed7320db4765aa5804ee6348e6017a2a
Maxim Nawangwe <>
2021-05-13 11:47:49 +0800

    introduced theming

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import App from 'next/app';
 import {Provider as StyletronProvider} from 'styletron-react';
-import {LightTheme, DarkTheme, BaseProvider} from 'baseui';
+import {BaseProvider} from 'baseui';
 import {styletron} from '../styletron';
+import DefaultTheme from '../themes/default'
 
 export default class MyApp extends App {
   render() {
     const {Component, pageProps} = this.props;
     return (
       <StyletronProvider value={styletron}>
-        <BaseProvider theme={LightTheme}>
+        <BaseProvider theme={DefaultTheme}>
           <Component {...pageProps} />
         </BaseProvider>
       </StyletronProvider>
