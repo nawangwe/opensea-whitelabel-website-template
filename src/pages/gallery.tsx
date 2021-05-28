165eec248dcae806cab07aea65737da57b24ab41
Maxim Nawangwe <>
2021-05-28 21:10:23 +0800

    Added a gallery details page

--- src/pages/gallery.tsx
+++ src/pages/gallery.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Card, StyledBody } from "baseui/card";
+import { Card, StyledAction, StyledBody } from "baseui/card";
 import {
   Paragraph1
 } from 'baseui/typography';
@@ -11,6 +11,7 @@ import * as Web3 from 'web3'
 import { OpenSeaPort, Network } from 'opensea-js'
 import { OpenSeaAsset } from 'opensea-js/lib/types';
 import Page from '../containers/page';
+import { Button } from 'baseui/button';
 
 interface GalleryProps extends SizeMeProps {
   assets: OpenSeaAsset[]
@@ -47,9 +48,6 @@ function Gallery ({assets, size}: GalleryProps) {
   return (
     <div>
         <Page pageRoute="gallery">
-          <Paragraph1 marginTop="scale1000">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </Paragraph1>
           {/* <HeaderImage /> */}
           <StackGrid
             style={{marginTop: 50}}
@@ -64,20 +62,8 @@ function Gallery ({assets, size}: GalleryProps) {
                 <Link href={`/gallery/${asset.tokenAddress}/${asset.tokenId}`}>
                     <Card 
                       overrides={{HeaderImage: {style: {width: '95%', padding: '2.5%'}}}}
-                      headerImage={asset.imageUrl}
+                      headerImage={asset.imageUrl.replace('s250', 's600')}
                       title={asset.name}>
-                      <StyledBody>
-                        {asset.description}
-                      </StyledBody>
-                      {/* <StyledAction>
-                        <Button
-                          overrides={{
-                            BaseButton: { style: { width: "100%" } }
-                          }}
-                        >
-                          Button Label
-                        </Button>
-                      </StyledAction> */}
                     </Card>
                 </Link>
               </div>
