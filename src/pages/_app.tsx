41548f1ef92717676d2854fc89a3665f411d671c
Maxim Nawangwe <>
2021-06-04 03:10:03 +0800

    Added progress bar for loading indicators, added duplicate order filtering

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,11 @@ import {BaseProvider, DarkTheme} from 'baseui';
 import {styletron} from '../styletron';
 import DefaultTheme from '../themes/default'
 import Context from '../context';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());  
 
 function MyApp({ Component, pageProps }) {
     const darkMode = useDarkMode()
