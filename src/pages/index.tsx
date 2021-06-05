a7825c14765ef8819a43fa2b800f2c22b29f33b0
Maxim Nawangwe <>
2021-06-06 00:13:43 +0800

    - removed about page - added image viewer - modified nav bar to cater to mobile devices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,18 +6,16 @@ import {
   Paragraph1
 } from 'baseui/typography';
 import StackGrid from "react-stack-grid";
-import sizeMe, { SizeMeProps } from 'react-sizeme'
+import { SizeMeProps, withSize } from 'react-sizeme'
 import Link from 'next/link'
-
-
 import * as Web3 from 'web3'
 import { OpenSeaPort, Network } from 'opensea-js'
-import { Order } from 'opensea-js/lib/types';
+import { OpenSeaAsset } from 'opensea-js/lib/types';
 import Page from '../containers/page';
 import NFTCard from '../components/nftcard';
 
 interface IndexProps extends SizeMeProps {
-  orders: Order[]
+  assets: OpenSeaAsset[]
 }
 
 export async function getServerSideProps() {
@@ -28,56 +26,59 @@ export async function getServerSideProps() {
     networkName: Network.Main
   })
 
-  const response: { orders: Order[], count: number; } = await seaport.api.getOrders({
-    maker: process.env.OPEN_SEA_WALLET_ADDRESS
-  })
+  const response: { assets: OpenSeaAsset[], estimatedCount: number; } = await seaport.api.getAssets({
+    collection_slug: process.env.OPEN_SEA_COLLECTION_SLUG
+  } as any)
 
-  const orders = JSON.parse(JSON.stringify(response)).orders
+  const assets = JSON.parse(JSON.stringify(response)).assets
 
-  return { props: { orders: orders.filter((v,i,a)=>a.findIndex(t=>(t.asset.tokenId === v.asset.tokenId))===i) }} // getting rid of duplicate entires in the order book
+  // sort items not on sale to bottom
+  assets.sort(function(a,b){ 
+    if(a.sellOrders != null) return -1;
+    else if(a.sellOrders === null) return 1
+  });
+
+  return { props: { assets: assets } }
 }
 
-function Index ({orders, size}: IndexProps) {
+function Index ({assets, size}: IndexProps) {
 
   const [css, theme] = useStyletron()
 
   return (
-    <div>
-        <Page pageRoute="home">
-          <Paragraph1 marginTop="scale1000">
-            {process.env.NEXT_PUBLIC_INTRODUCTORY_TEXT}
-            </Paragraph1>
+    <Page pageRoute="home">
+      <Paragraph1 marginTop="scale1000">
+        {process.env.NEXT_PUBLIC_INTRODUCTORY_TEXT}
+        </Paragraph1>
 
-          <HeadingSmall>Gallery</HeadingSmall>
-          {/* <HeaderImage /> */}
-          <StackGrid
-            style={{marginTop: 50, zIndex: 0}}
-            columnWidth={size.width <= 768 ? '100%' : '33.33%'}
-            gutterWidth={50}
-            gutterHeight={50}
-            monitorImagesLoaded={true}
-          >
-            {orders.slice(0,3).map(order => {
-            return(
-              <div key={order.createdTime.toString()}>
-                <NFTCard order={order}/>
-              </div>
-            )
-          })}
-          </StackGrid>
-          <div className={css({
-            display: 'flex',
-            justifyContent: 'center',
-            marginTop: '30px'
-          })}>
-            <Link href="/gallery">
-              <Button>
-                See more
-              </Button>
-            </Link>
+      <HeadingSmall>Gallery</HeadingSmall>
+      <StackGrid
+        style={{marginTop: 50, zIndex: 0}}
+        columnWidth={size.width <= 768 ? '100%' : '33.33%'}
+        gutterWidth={50}
+        gutterHeight={50}
+        monitorImagesLoaded={true}
+      >
+        {assets.slice(0,3).map(asset => {
+        return(
+          <div key={asset.tokenId}>
+            <NFTCard asset={asset}/>
           </div>
-        </Page>
-    </div>
+        )
+      })}
+      </StackGrid>
+      <div className={css({
+        display: 'flex',
+        justifyContent: 'center',
+        marginTop: '30px'
+      })}>
+        <Link href="/gallery">
+          <Button>
+            See more
+          </Button>
+        </Link>
+      </div>
+    </Page>
   );
 };
-export default sizeMe()(Index);
+export default withSize()(Index);
