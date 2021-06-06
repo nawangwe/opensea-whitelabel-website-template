4df36c55a5831d1365239cfd00406a5862422a89
Maxim Nawangwe <>
2021-06-06 12:55:45 +0800

    - prettier formatting and connect button bug fix for mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,50 +1,94 @@
-import React from 'react';
-import {useStyletron} from 'baseui';
-import { BEHAVIOR, Cell, Grid } from 'baseui/layout-grid';
-import { FaTwitter } from 'react-icons/fa';
-import { FaInstagram } from 'react-icons/fa';
-import { FaGithub } from 'react-icons/fa';
-import { LabelSmall } from 'baseui/typography';
+import React from 'react'
+import {useStyletron} from 'baseui'
+import {BEHAVIOR, Cell, Grid} from 'baseui/layout-grid'
+import {FaTwitter} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
+import {LabelSmall} from 'baseui/typography'
 
-function Footer({size}) {
-  const [css, theme] = useStyletron();
+function Footer ({size}) {
+  const [css, theme] = useStyletron()
   return (
-    <div className={css({
+    <div
+      className={css({
         backgroundColor: theme.colors.primaryB,
         width: 100,
         paddingTop: theme.sizing.scale800,
         paddingBottom: theme.sizing.scale800,
-        boxShadow: 'rgb(0 0 0 / 8%) 0px -1px 0px'
-        })}>
-        <Grid behavior={BEHAVIOR.fixed} gridGaps={20} gridColumns={[6,6,12,12]} >
-            <Cell span={6} overrides={{Cell: {
-                style: {
-                    display: 'flex !important',
-                    flexDirection: 'column',
-                    alignItems: size.width <= 1136 ? 'center' : 'flex-start'
-                }
-            }}}>
-                <LabelSmall className={css({ color: theme.colors.contentPrimary})}>© 2021 Maxim Nawangwe</LabelSmall>
-                <div style={{marginTop: 10}}>
-                <a href={`${process.env.NEXT_PUBLIC_TWITTER}`} target="_blank"><FaTwitter style={{width: 30, height: 30}} color={theme.colors.contentPrimary}/></a>
-                <a href={`${process.env.NEXT_PUBLIC_INSTAGRAM}`} target="_blank"><FaInstagram href={process.env.NEXT_PUBLIC_INSTAGRAM} style={{width: 30, height: 30, marginLeft: 10}} color={theme.colors.contentPrimary}/></a>
-                <a href={`${process.env.NEXT_PUBLIC_GITHUB}`} target="_blank"><FaGithub style={{width: 30, height: 30, marginLeft: 10}} color={theme.colors.contentPrimary}/></a>
-                </div>
-            </Cell>
-            <Cell span={6} overrides={{Cell: {
-                style: {
-                    display: 'flex !important',
-                    flexDirection: 'column',
-                    alignItems: size.width <= 1136 ? 'center' : 'flex-end'
-                }
-            }}}>
-                <LabelSmall className={css({ color: theme.colors.contentPrimary})}>Created with OpenSea whitelable template</LabelSmall>
-                <div style={{marginTop: 10}}/>
-                <a href="https://github.com/nawangwe/opensea-whitelabel-website-template" target="_blank"><FaGithub style={{width: 30, height: 30}} color={theme.colors.contentPrimary}/></a>
-            </Cell>
-        </Grid>
+        boxShadow: 'rgb(0 0 0 / 8%) 0px -1px 0px',
+      })}
+    >
+      <Grid
+        behavior={BEHAVIOR.fixed}
+        gridGaps={20}
+        gridColumns={[6, 6, 12, 12]}
+      >
+        <Cell
+          span={6}
+          overrides={{
+            Cell: {
+              style: {
+                display: 'flex !important',
+                flexDirection: 'column',
+                alignItems: size.width <= 1136 ? 'center' : 'flex-start',
+              },
+            },
+          }}
+        >
+          <LabelSmall className={css({color: theme.colors.contentPrimary})}>
+            © 2021 Maxim Nawangwe
+          </LabelSmall>
+          <div style={{marginTop: 10}}>
+            <a href={`${process.env.NEXT_PUBLIC_TWITTER}`} target='_blank'>
+              <FaTwitter
+                style={{width: 30, height: 30}}
+                color={theme.colors.contentPrimary}
+              />
+            </a>
+            <a href={`${process.env.NEXT_PUBLIC_INSTAGRAM}`} target='_blank'>
+              <FaInstagram
+                href={process.env.NEXT_PUBLIC_INSTAGRAM}
+                style={{width: 30, height: 30, marginLeft: 10}}
+                color={theme.colors.contentPrimary}
+              />
+            </a>
+            <a href={`${process.env.NEXT_PUBLIC_GITHUB}`} target='_blank'>
+              <FaGithub
+                style={{width: 30, height: 30, marginLeft: 10}}
+                color={theme.colors.contentPrimary}
+              />
+            </a>
+          </div>
+        </Cell>
+        <Cell
+          span={6}
+          overrides={{
+            Cell: {
+              style: {
+                display: 'flex !important',
+                flexDirection: 'column',
+                alignItems: size.width <= 1136 ? 'center' : 'flex-end',
+              },
+            },
+          }}
+        >
+          <LabelSmall className={css({color: theme.colors.contentPrimary})}>
+            Created with OpenSea whitelable template
+          </LabelSmall>
+          <div style={{marginTop: 10}} />
+          <a
+            href='https://github.com/nawangwe/opensea-whitelabel-website-template'
+            target='_blank'
+          >
+            <FaGithub
+              style={{width: 30, height: 30}}
+              color={theme.colors.contentPrimary}
+            />
+          </a>
+        </Cell>
+      </Grid>
     </div>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
